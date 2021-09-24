1af416223d126d33cf97bb4d0d2520a3622093bb
jeheidl <jens.heidl@daimler.com>
2021-09-24 08:51:51 +0200

    init

---
+++ src/SomeTestComp.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useRef, useState} from 'react';
+
+const SomeTestComp = (props) =>
+{
+  return (
+    <>
+        <h1>Heppa Test</h1>
+    </>
+  );
+};
+
+export {SomeTestComp};
